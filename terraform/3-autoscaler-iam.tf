3e7a37a7d3e8c429b4c180f05a9e1a369abae816
Joaquin Fernandez <jnfernandez.ojeda@gmail.com>
2023-04-22 03:27:35 -0300

    Autoscaler version

--- terraform/3-autoscaler-iam.tf
+++ terraform/3-autoscaler-iam.tf
@@ -1,6 +1,6 @@
 module "cluster_autoscaler_irsa_role" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version = "5.3.1"
+  version = "5.17.0"
 
   role_name                        = "cluster-autoscaler"
   attach_cluster_autoscaler_policy = true
