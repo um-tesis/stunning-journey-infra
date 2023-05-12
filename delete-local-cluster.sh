148b8d76fe40dd6be74add2a2d55fe2dfe5e0df3
Joaquin Fernandez <jnfernandez.ojeda@gmail.com>
2023-05-12 20:46:55 -0300

    local-cluster working

--- delete-local-cluster.sh
+++ delete-local-cluster.sh
@@ -3,14 +3,10 @@
 set -e
 
 yaml_files=(
-    ./k8s-resources/local-minikube-cluster/09-deploymentAndService-frontend.yaml
-    ./k8s-resources/local-minikube-cluster/08-deploymentAndService-backend.yaml
-    ./k8s-resources/local-minikube-cluster/07-configMap-backend.yaml
-    ./k8s-resources/local-minikube-cluster/06-service-postgres.yaml
-    ./k8s-resources/local-minikube-cluster/05-deployment-postgres.yaml
-    ./k8s-resources/local-minikube-cluster/04-persistentVolumeClaim-postgres.yaml
-    ./k8s-resources/local-minikube-cluster/03-persistenVolume-postgres.yaml
-    ./k8s-resources/local-minikube-cluster/02-configMap-postgres.yaml
+    ./k8s-resources/local-minikube-cluster/05-deploymentAndService-frontend.yaml
+    ./k8s-resources/local-minikube-cluster/04-deploymentAndService-backend.yaml
+    ./k8s-resources/local-minikube-cluster/03-configMap-backend.yaml
+    ./k8s-resources/local-minikube-cluster/02-deploymentAndService-postgres.yaml
     ./k8s-resources/local-minikube-cluster/01-namespaces.yaml
 )
 
